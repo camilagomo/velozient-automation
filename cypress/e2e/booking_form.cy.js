85075f0a05c4c188591bb47fe2aa139f4f72dbd2
camilagomo <camilagomo@hotmail.com>
2025-07-11 21:26:54 -0300

    update file booking_form

--- cypress/e2e/booking_form.cy.js
+++ cypress/e2e/booking_form.cy.js
@@ -3,15 +3,14 @@ Cypress.on('uncaught:exception', () => false);
 
 describe('Booking room Selection and API Validation', () => {
   it('Should select valid dates and validate API calls', () => {
-    // Visit the homepage
-    cy.visit('https://automationintesting.online/');
-
     const checkInDate = '2025-07-12';
     const checkOutDate = '2025-07-14';
 
-    // Intercept only the actual calls you confirmed
+    // Intercept expected API calls BEFORE visiting the page
     cy.intercept('GET', '**/room**').as('getRooms');
-    cy.intercept('GET', '**/branding**').as('getBranding');
+
+    // Visit the homepage
+    cy.visit('https://automationintesting.online/');
 
     // Fill in check-in and check-out dates
     cy.get(':nth-child(1) > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control')
@@ -22,13 +21,12 @@ describe('Booking room Selection and API Validation', () => {
     // Click on the "Check Availability" button
     cy.contains('Check Availability').click();
 
-    // Wait for those
-    cy.wait('@getRooms');
-    cy.wait('@getBranding');
+    // Wait for the room data to load
+    cy.wait('@getRooms', { timeout: 10000 });
 
     // Click on the first available "Book now" button
     cy.contains('Book now', { timeout: 10000 }).should('exist');
-    cy.contains('Book now').should('exist').first().click();
+    cy.contains('Book now').first().click();
 
     // Assert that the user is redirected to the reservation page
     cy.url().should('include', '/reservation/');
